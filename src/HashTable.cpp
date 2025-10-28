8b3c7dcb486fed2119f53846972f49a00212d29a
VicentDev <175661774+Vicent-deVv@users.noreply.github.com>
2025-10-28 04:32:58 +0000

    Se agregaron las funciones hash

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -11,7 +11,24 @@ using namespace std;
 // INTEGRANTE 3: Método de redimensionamiento
 
 
-// INTEGRANTE 4: Funciones hash
-
+int firstHash(string word, int size){
+            int hash = 0;
+            
+            for(int i = 0; i < word.size(); i++){
+                hash = hash + (word[i] * word[i]);
+            }
+            
+            return hash % size;
+        }
+        
+        int secondHash(string word, int size){
+            int hash = 0;
+            
+            for(int i = 0; i < word.size(); i++){
+                hash = hash + ((2 * word[i]) * (2 * word[i]));
+            }
+            
+            return hash % size;
+        }
 
 // INTEGRANTE 4: Método de impresión
